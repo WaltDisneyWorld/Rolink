d77efd702fb1264e70648cec905479a5e9f51009
Tigerism <Tigerism@users.noreply.github.com>
2018-05-14 22:06:26 -0400

    Ready event

--- resources/events/ready.py
+++ resources/events/ready.py
@@ -1,6 +1,9 @@
 from resources.framework import client
+from discord import Game, Status
 
 
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}', flush=True)
+    game = Game("!help | !invite")
+    await client.change_presence(status=Status.online, activity=game)
