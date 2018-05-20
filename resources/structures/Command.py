09face72522b93d0542742f86f19a34d0d70406a
Tigerism <Tigerism@users.noreply.github.com>
2018-05-20 16:03:05 -0400

    Command structure change

--- resources/structures/Command.py
+++ resources/structures/Command.py
@@ -1,9 +1,13 @@
 class Command:
-	def __init__(self, func, name=None, **kwargs):
+	def __init__(self, func, name=None, is_subcommand=False, **kwargs):
 		self.name = name or func.__name__
-		self.subcommands = []
+		self.subcommands = {}
 		self.description = func.__doc__ or "N/A"
 		self.aliases = kwargs.get("alias") or kwargs.get("aliases") or list()
 		self.permissions = kwargs.get("permissions", list())
-		self.arguments = kwargs.get("arguments", list())
+		self.arguments = kwargs.get("arguments") or kwargs.get("args") or list()
+		self.category = kwargs.get("category", "Miscellaneous")
+		self.is_subcommand = is_subcommand
 		self.func = func
+	def add_subcommand(self, command):
+		self.subcommands[command.name] = command
