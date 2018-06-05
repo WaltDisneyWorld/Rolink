c4cbca65daa272ffacdb0aa2dbc517c2699e8f9c
Tigerism <Tigerism@users.noreply.github.com>
2018-06-04 22:59:07 -0400

    RobloxUser class

---
+++ resources/structures/RobloxUser.py
@@ -0,0 +1,38 @@
+get_details = None
+
+class RobloxUser:
+	def __init__(self, username=None, id=None, **kwargs):
+		self.username = username
+		self.id = id
+
+		self.is_complete = False
+		self.is_verified = False
+
+		self.groups = kwargs.get("groups", {})
+		self.avatar = kwargs.get("avatar")
+		self.membership = kwargs.get("membership")
+		self.presence = kwargs.get("presence")
+
+	async def fill_missing_details(self, complete=False):
+		global get_details
+		if not get_details:
+			from resources.modules.roblox import get_details
+
+		if not self.is_verified or (complete and not self.is_complete):
+			data = await get_details(
+				username=self.username,
+				id = self.id,
+				complete = complete
+			)
+
+			if data["username"] and data["id"]:
+				self.username = data["username"]
+				self.id = data["id"]
+				self.is_verified = True
+
+		if not self.is_complete and complete:
+			self.groups = data["extras"].get("groups")
+			self.avatar = data["extras"].get("avatar")
+			self.membership = data["extras"].get("membership")
+			self.presence = data["extras"].get("presence")
+			self.is_complete = True
--- resources/structures/__init__.py
+++ resources/structures/__init__.py
@@ -2,3 +2,4 @@ from .Command import Command
 from .Module import Module
 from .Response import Response
 from .Argument import Argument, parse_flags
+from .RobloxUser import RobloxUser
