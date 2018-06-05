e77267961fc400e828487e42f5b88e2dfdc8a0b4
Tigerism <Tigerism@users.noreply.github.com>
2018-06-04 23:00:34 -0400

    Added more Response send methods

--- resources/structures/Response.py
+++ resources/structures/Response.py
@@ -1,3 +1,5 @@
+from discord.errors import Forbidden
+
 class Response:
 	def __init__(self, message, command="N/A"):
 		self.message = message
@@ -8,12 +10,12 @@ class Response:
 		channel = dm and self.author or self.channel
 		try:
 			await channel.send(embed=embed, content=content)
-		except:
+		except Forbidden:
 			# try to send on_error
 			# if that fails, try to post in opposite channel that it couldn't send a msg
 			try:
 				await channel.send(content=on_error or content)
-			except:
+			except Forbidden:
 				channel = dm and self.channel or self.author # opposite channel
 				try:
 					if dm:
@@ -21,11 +23,18 @@ class Response:
 						"Please check your privacy settings and try again.")
 					else:
 						await self.author.send(f'You attempted to use command {self.command} in '
-						f'{self.channel.mention}, but I was unable to post there.')
-				except:
+						f'{self.channel.mention}, but I was unable to post there. ' \
+							"You may need to grant me the ``Embed Links`` permission.")
+				except Forbidden:
 					pass
 	async def error(self, error, embed=None, embed_color=0xE74C3C):
 		if embed:
 			embed.color = embed_color
-		await self.send(error, embed=embed)
-	   
+		await self.send(":exclamation: " + error, embed=embed)
+	async def success(self, success, embed=None, embed_color=0x3CE754):
+		if embed:
+			embed.color = embed_color
+		await self.send(":thumbsup: " + success, embed=embed)
+	async def text(self, text, embed=None):
+		await self.send(text, embed=embed)
+  
