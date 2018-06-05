bc60cbe5b69d91a639c5ecf7699c0e1ae41ce6af
Tigerism <Tigerism@users.noreply.github.com>
2018-06-04 22:55:52 -0400

    Rotating game name

--- resources/events/ready.py
+++ resources/events/ready.py
@@ -1,4 +1,7 @@
 from discord import Game, Status
+from asyncio import sleep
+from resources.settings import GAME as game_list
+
 
 
 async def setup(client, *args, **kwargs):
@@ -6,5 +9,12 @@ async def setup(client, *args, **kwargs):
 	@client.event
 	async def on_ready():
 		print(f'Logged in as {client.user}', flush=True)
-		game = Game("!help | !invite")
-		await client.change_presence(status=Status.online, activity=game)
+		while True:
+			for game in game_list:
+				game_name = game.format(
+					guilds=len(client.guilds),
+					users=len(client.users)
+				)
+				await client.change_presence(status=Status.online, activity=Game(game_name))
+				await sleep(20)
+
