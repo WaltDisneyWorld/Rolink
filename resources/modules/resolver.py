4c4787929cfd14b98b83d4644fbb6f57612eac40
Tigerism <Tigerism@users.noreply.github.com>
2018-06-04 23:03:04 -0400

    Added choice resolver

--- resources/modules/resolver.py
+++ resources/modules/resolver.py
@@ -1,9 +1,20 @@
-def string_resolver(message, content=None, arg=None):
+def string_resolver(message, arg, content=None):
 	if not content:
 		content = message.content
 
-	return str(content)
+	return str(content), None
+
+def choice_resolver(message, arg, content=None):
+	if not content:
+		content = message.content
+
+	for choice in arg["choices"]:
+		if choice.lower() == content.lower():
+			return choice, None
+
+	return False, f'Choice must be of either: {str(arg["choices"])}'
 
 resolver_map = {
-	"string": string_resolver
+	"string": string_resolver,
+	"choice": choice_resolver
 }
