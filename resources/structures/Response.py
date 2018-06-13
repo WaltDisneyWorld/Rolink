99abcbcccf63ca61173b03ec84b9bf860ee7fddc
Tigerism <Tigerism@users.noreply.github.com>
2018-06-13 00:45:46 -0400

    Misc response changes

--- resources/structures/Response.py
+++ resources/structures/Response.py
@@ -7,37 +7,39 @@ class Response:
 		self.author = message.author
 		self.command = command
 		self.guild = message.guild
-	async def send(self, content=None, embed=None, on_error=None, dm=False):
+	async def send(self, content=None, embed=None, on_error=None, dm=False, dm_post=False, strict_post=False):
 		channel = dm and self.author or self.channel
-		if embed and hasattr(self.guild.me, "color"):
+		if embed and not dm:
 			embed.color = self.guild.me.color
 		try:
 			await channel.send(embed=embed, content=content)
-			if dm:
-				await self.channel.send("**Check your DMs!**")
+			if dm_post:
+				await self.channel.send(self.author.mention + ", **check your DMs!**")
 		except Forbidden:
-			channel = dm and self.channel or self.author # opposite channel
+			channel = not strict_post and (dm and self.channel or self.author) or channel # opposite channel
 			try:
 				await channel.send(content=on_error or content, embed=embed)
 			except Forbidden:
 				try:
 					if dm:
-						await self.channel.send(content=str(self.author) + ", I was unable to DM you. "
+						await self.channel.send(content= self.author.mention + ", I was unable to DM you. "
 						"Please check your privacy settings and try again.")
 					else:
 						await self.author.send(f'You attempted to use command {self.command} in '
 						f'{self.channel.mention}, but I was unable to post there. ' \
 							"You may need to grant me the ``Embed Links`` permission.")
+					return False
 				except Forbidden:
-					pass
-	async def error(self, error, embed=None, embed_color=0xE74C3C):
-		if embed:
+					return False
+		return True
+	async def error(self, error, embed=None, embed_color=0xE74C3C, dm=False):
+		if embed and not dm:
 			embed.color = embed_color
-		await self.send(":exclamation: " + error, embed=embed)
-	async def success(self, success, embed=None, embed_color=0x3CE754):
-		if embed:
+		return await self.send(":exclamation: " + error, embed=embed, dm=dm)
+	async def success(self, success, embed=None, embed_color=0x3CE754, dm=False):
+		if embed and not dm:
 			embed.color = embed_color
-		await self.send(":thumbsup: " + success, embed=embed)
-	async def text(self, text, embed=None):
-		await self.send(text, embed=embed)
+		return await self.send(":thumbsup: " + success, embed=embed, dm=dm)
+	async def text(self, text, dm=False):
+		return await self.send(text, dm=dm)
   
