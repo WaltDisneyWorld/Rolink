539a5ea59af2946d4c936483c178e9ab6e2de816
Tigerism <Tigerism@users.noreply.github.com>
2018-06-14 01:59:48 -0400

    Add number, channel, and role resolvers

--- resources/modules/resolver.py
+++ resources/modules/resolver.py
@@ -4,7 +4,36 @@ def string_resolver(message, arg, content=None):
 	if not content:
 		content = message.content
 
+	min = arg.get("min", -100)
+	max = arg.get("max", 100)
+
+	if arg.get("min") or arg.get("max"):
+		if min <= len(content) <= max:
+			return str(content), None
+		else:
+			return False, f'String character count not in range: {min}-{max}'
+
 	return str(content), None
+
+def number_resolver(message, arg, content=None):
+	if not content:
+		content = message.content
+
+	if content.isdigit():
+		min = arg.get("min", -100)
+		max = arg.get("max", 100)
+
+		if arg.get("min") or arg.get("max"):
+			if min <= len(content) <= max:
+				return int(content), None
+			else:
+				return False, f'Number character count not in range: {min}-{max}'
+		else:
+			return int(content), None
+
+		return int(content), None
+
+	return False, "You must pass a number"
 
 def choice_resolver(message, arg, content=None):
 	if not content:
@@ -64,9 +93,35 @@ def channel_resolver(message, arg, content=None):
 
 	return False, "Invalid channel"
 
+def role_resolver(message, arg, content=None):
+	if not content:
+		content = message.content
+
+	guild = message.guild
+
+	if message.role_mentions:
+		return message.role_mentions[0], None
+	else:
+		is_int, is_id = None, None
+
+		try:
+			is_int = int(content)
+			is_id = is_int > 15
+		except ValueError:
+			pass
+
+		if is_id:
+			return find(lambda r: r.id == is_int, guild.roles), None
+		else:
+			return find(lambda r: r.name == content, guild.roles), None
+
+	return False, "Invalid role"
+
 resolver_map = {
 	"string": string_resolver,
+	"number": number_resolver,
 	"choice": choice_resolver,
 	"user": user_resolver,
-	"channel": channel_resolver
+	"channel": channel_resolver,
+	"role": role_resolver,
 }
