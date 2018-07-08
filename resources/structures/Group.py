85e1662bb0dd5554e268c8bc37c7d7e1e1e76b35
Tigerism <Tigerism@users.noreply.github.com>
2018-07-08 00:03:01 -0400

    Structures.Group can parse more options

--- resources/structures/Group.py
+++ resources/structures/Group.py
@@ -1,25 +1,29 @@
 class Group:
-    def __init__(self, id=None, **kwargs):
-        self.id = id and str(id)
+	def __init__(self, id=None, **kwargs):
+		self.id = id and str(id)
 
-        self.name = None
-        self.description = None
-        self.roles = None
-        self.owner = None
-        
-        self.user_rank = None
-        self.user_role = None
+		self.name = None
+		self.description = None
+		self.roles = None
+		self.owner = None
+		self.member_count = None
+		
+		self.user_rank = None
+		self.user_role = None
 
-        self.load_json(**kwargs)
+		self.load_json(**kwargs)
 
-    def load_json(self, **json):
-        self.id = self.id or str(json["Id"])
-        self.name = self.name or (json.get("Name") and u'{}'.format(json.get("Name")).encode("utf-8").strip())
-        self.description = self.description or json.get("Description", "N/A")
-        self.roles = self.roles or json.get("Roles")
-        self.owner = self.owner or json.get("Owner")
+	def load_json(self, **json):
+		self.id = self.id or str(json["Id"])
+		self.name = self.name or json.get("Name")
+		self.description = self.description or json.get("Description") or json.get("description", "N/A")
+		self.roles = self.roles or json.get("Roles")
+		self.owner = self.owner or json.get("Owner") or json.get("owner")
+		self.member_count = self.member_count or json.get("memberCount")
 
-        self.user_rank = self.user_rank or json.get("Rank")
-        self.user_role = self.user_role or json.get("Role")
-    def __str__(self):
-        return "{} ({})".format(self.name, self.id)
+		self.user_rank = self.user_rank or json.get("Rank")
+		self.user_role = self.user_role or json.get("Role")
+	def __str__(self):
+		name = u'{}'.format(self.name).encode("utf-8").strip()
+
+		return "{} ({})".format(name, self.id)
