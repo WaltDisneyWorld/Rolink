845156c412a928e4839b514f0078b7cff8c67657
Tigerism <Tigerism@users.noreply.github.com>
2018-07-14 21:53:49 -0400

    Leave clantag blank to delete value

--- commands/clantag.py
+++ commands/clantag.py
@@ -1,4 +1,5 @@
 from resources.modules.roblox import get_nickname
+from resources.modules.utils import post_event
 from discord.errors import Forbidden
 
 async def setup(**kwargs):
@@ -11,37 +12,49 @@ async def setup(**kwargs):
 			"type": "string",
 			"name": "clan_tag",
 			"min": 1,
-			"max": 10
+			"max": 10,
+			"optional": True
 		}
 	], category="Account")
 	async def on_clan_tag(message, response, args):
-		"""assign a clan tag for the server"""
+		"""assign a clan tag for the server. Leave the value blank to clear clan tag."""
 
 		author = message.author
 		guild = message.guild
 
 		clan_tags = (await r.table("users").get(str(author.id)).run() or {}).get("clanTags", {})
 
-		clan_tags[str(guild.id)] = args.parsed_args["clan_tag"]
+		clan_tags[str(guild.id)] = args.parsed_args.get("clan_tag") or None
+
+		msg = None
 
 		await r.table("users").insert({
 			"id": str(author.id),
 			"clanTags": clan_tags
 		}, conflict="update").run()
 
-		msg = await response.success("Saved your new clan tag! If this server supports " \
-			"clan tags, and you're linked to the bot, you'll be given it automatically.")
+		if args.parsed_args.get("clan_tag"):
+			msg = await response.success("Saved your new clan tag! If this server supports " \
+				"clan tags, and you're linked to the bot, you'll be given it automatically. " \
+				"Call this command again without arguments to remove the clan tag.")
+		else:
+			msg = await response.success("Successfully **removed** your clan tag.")
 
 		nickname = await get_nickname(author)
 		if nickname:
 			if author.nick != nickname:
 				try:
 					await author.edit(nick=nickname)
-					msg.edit(content="Saved your new clan tag! Your nickname has also been updated " \
-						"successfully.")
 				except Forbidden:
 					if msg:
 						await msg.edit(content=msg.content + "\n**Error:** I was unable to edit your nickname.")
+						await post_event(
+							"error",
+							f"Failed to update {author.mention}'s nickname. Please ensure I have " \
+								"the ``Manage Nickname`` permission, and drag my role above the other roles.",
+							guild=guild,
+							color=0xE74C3C
+						)
 
 
 
