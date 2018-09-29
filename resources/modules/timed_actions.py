7cac7de1236b4d4ee6a1015b5662e015da20963d
Tigerism <Tigerism@users.noreply.github.com>
2018-09-29 19:25:04 -0400

    Catch unknown shoutproxy exceptions

--- resources/modules/timed_actions.py
+++ resources/modules/timed_actions.py
@@ -23,112 +23,116 @@ class TimedActions:
 
 			while await feed.fetch_next():
 				guild = await feed.next()
-				actual_guild = self.client.get_guild(int(guild.get("actualId", "1")))
 
-				if actual_guild:
-					is_p, _, _, _, _ = await is_premium(guild=actual_guild)
+				try:
+					actual_guild = self.client.get_guild(int(guild.get("actualId", "1")))
 
-					if is_p:
-						group_id = str(guild.get("groupID"))
+					if actual_guild:
+						is_p, _, _, _, _ = await is_premium(guild=actual_guild)
 
-						if group_id:
-							group_shout = guild.get("groupShout", {})
-							channel = int(group_shout.get("channel", 0))
-							channel = self.client.get_channel(channel)
+						if is_p:
+							group_id = str(guild.get("groupID"))
 
-							if channel:
+							if group_id:
+								group_shout = guild.get("groupShout", {})
+								channel = int(group_shout.get("channel", 0))
+								channel = self.client.get_channel(channel)
 
-								try:
-									shout = await get_group_shout(group_id)
-								except GroupNotFound:
-									continue
-								
-								if not shout.get("shout"):
-									continue
+								if channel:
 
-								datetime = dateutil.parser.parse(shout["shout"]["updated"])
+									try:
+										shout = await get_group_shout(group_id)
+									except GroupNotFound:
+										continue
+									
+									if not shout.get("shout"):
+										continue
 
-								if group_shout.get("lastShout") != shout["shout"]["updated"]:
-									group_shout["lastShout"] = shout["shout"]["updated"]
+									datetime = dateutil.parser.parse(shout["shout"]["updated"])
 
-									await self.r.table("guilds").get(guild["id"]).update({
-										"groupShout": group_shout
-									}).run()
+									if group_shout.get("lastShout") != shout["shout"]["updated"]:
+										group_shout["lastShout"] = shout["shout"]["updated"]
 
-									group = await get_group(group_id)
+										await self.r.table("guilds").get(guild["id"]).update({
+											"groupShout": group_shout
+										}).run()
 
-									if group_shout.get("default"):
-										embed = Embed(
-											title="Group Shout",
-											description=f'Shouted by: {shout["shout"]["poster"]["username"]}',
-											timestamp=datetime
-										)
+										group = await get_group(group_id)
 
-										embed.set_author(
-											name=shout["name"],
-											icon_url=group.embed_url,
-											url=group.url
-										)
-
-										embed.add_field(
-											name="Shout:",
-											value=shout["shout"]["body"]
-										)
-
-										if group_shout.get("prependContent"):
-											try:
-												await channel.send(content=group_shout.get("prependContent"), embed=embed)
-											except DiscordException:
-												pass
-										else:
-											try:
-												await channel.send(embed=embed)
-											except DiscordException:
-												pass
-									else:
-										if "{group-rank}" in group_shout["format"]:
-											user, _ = await get_user(id=shout["shout"]["poster"]["userId"])
-											await user.fill_missing_details()
-
-											user_group = user.groups.get(group_id)
-
-											if user_group:
-												group_rank = user_group.user_role
-											else:
-												group_rank = "Guest"
-										else:
-											group_rank = "Guest"
-
-										if group_shout.get("cleanContent"):
-											new_shout = shout["shout"]["body"].replace("@", "@\u200b")
-										else:
-											new_shout = shout["shout"]["body"]
-
-										if new_shout:
-
-											message = group_shout["format"].replace(
-												"{group-name}", shout["name"]
-											).replace(
-												"{group-shout}", new_shout
-											).replace(
-												"{group-id}", group_id
-											).replace(
-												"{roblox-name}", shout["shout"]["poster"]["username"]
-											).replace(
-												"{roblox-id}", str(shout["shout"]["poster"]["userId"])
-											).replace(
-												"{group-rank}", group_rank
+										if group_shout.get("default"):
+											embed = Embed(
+												title="Group Shout",
+												description=f'Shouted by: {shout["shout"]["poster"]["username"]}',
+												timestamp=datetime
 											)
 
-											try:
-												await channel.send(message)
-											except DiscordException:
-												pass
-						else:
-							# remove from db
-							pass
+											embed.set_author(
+												name=shout["name"],
+												icon_url=group.embed_url,
+												url=group.url
+											)
 
-			await sleep(60)
+											embed.add_field(
+												name="Shout:",
+												value=shout["shout"]["body"]
+											)
+
+											if group_shout.get("prependContent"):
+												try:
+													await channel.send(content=group_shout.get("prependContent"), embed=embed)
+												except DiscordException:
+													pass
+											else:
+												try:
+													await channel.send(embed=embed)
+												except DiscordException:
+													pass
+										else:
+											if "{group-rank}" in group_shout["format"]:
+												user, _ = await get_user(id=shout["shout"]["poster"]["userId"])
+												await user.fill_missing_details()
+
+												user_group = user.groups.get(group_id)
+
+												if user_group:
+													group_rank = user_group.user_role
+												else:
+													group_rank = "Guest"
+											else:
+												group_rank = "Guest"
+
+											if group_shout.get("cleanContent"):
+												new_shout = shout["shout"]["body"].replace("@", "@\u200b")
+											else:
+												new_shout = shout["shout"]["body"]
+
+											if new_shout:
+
+												message = group_shout["format"].replace(
+													"{group-name}", shout["name"]
+												).replace(
+													"{group-shout}", new_shout
+												).replace(
+													"{group-id}", group_id
+												).replace(
+													"{roblox-name}", shout["shout"]["poster"]["username"]
+												).replace(
+													"{roblox-id}", str(shout["shout"]["poster"]["userId"])
+												).replace(
+													"{group-rank}", group_rank
+												)
+
+												try:
+													await channel.send(message)
+												except DiscordException:
+													pass
+							else:
+								# remove from db
+								pass
+				except:
+					await sleep(60)
+
+			await sleep(5)
 
 	async def setup(self):
 		await self.group_shouts()
