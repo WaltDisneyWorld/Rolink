186e43d9cc10414ad7646c7ef75ffbdd2076bad9
tigerism <tigerism@users.noreply.github.com>
2019-06-30 13:45:46 -0400

    Add new operations to Arg containers

--- src/resources/structures/Args.py
+++ src/resources/structures/Args.py
@@ -11,5 +11,11 @@ class Args:
             setattr(self, name, value)
             self._items[name] = str(value)
 
+    def remove(self, item, no_error=True):
+        self._items.pop(item, no_error)
+
+    def clear(self):
+        self._items.clear()
+
     def __str__(self):
         return f"< Argument Holder: [{', '.join([x + ' -> ' + y for x, y in self._items.items()])}]"
