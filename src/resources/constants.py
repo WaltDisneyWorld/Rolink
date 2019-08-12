e72a9fdfc4e556c61440ae73bfdc087c3c56d3b4
tigerism <tigerism@users.noreply.github.com>
2019-08-11 23:55:21 -0400

    Add nickname templates to constants file

--- src/resources/constants.py
+++ src/resources/constants.py
@@ -1,2 +1,8 @@
 RED_COLOR = 0xE74C3C
 INVISIBLE_COLOR = 0x36393E
+
+NICKNAME_TEMPLATES = (
+    "{roblox-name} \u2192 changes to their Roblox username\n"
+    "{roblox-id} \u2192 changes to their Roblox user ID\n"
+    "{group-rank} \u2192 changes to their group rank"
+)
