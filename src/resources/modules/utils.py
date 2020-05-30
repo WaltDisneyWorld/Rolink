4894e5eed097d460c2689e23d2ca86d26b7d2735
tigerism <tigerism@users.noreply.github.com>
2020-05-29 21:46:59 -0400

    Clean-up

--- src/resources/modules/utils.py
+++ src/resources/modules/utils.py
@@ -16,255 +16,269 @@ is_patron = Bloxlink.get_module("patreon", attrs="is_patron")
 
 @Bloxlink.module
 class Utils(Bloxlink.Module):
-	def __init__(self):
-		self.option_regex = compile("(.+):(.+)")
-		self.bloxlink_server = self.client.get_guild(372036754078826496)
+    def __init__(self):
+        self.option_regex = compile("(.+):(.+)")
+        self.bloxlink_server = self.client.get_guild(372036754078826496)
 
 
-	async def __setup__(self):
-		try:
-			self.bloxlink_server = self.bloxlink_server or self.client.get_guild(372036754078826496) or await self.client.fetch_guild(372036754078826496)
-		except Forbidden:
-			self.bloxlink_server = None
+    async def __setup__(self):
+        try:
+            self.bloxlink_server = self.bloxlink_server or self.client.get_guild(372036754078826496) or await self.client.fetch_guild(372036754078826496)
+        except Forbidden:
+            self.bloxlink_server = None
 
-	@staticmethod
-	def get_files(directory):
-		return [name for name in listdir(directory) if name[:1] != "." and name[:2] != "__" and name != "_DS_Store"]
+    @staticmethod
+    def get_files(directory):
+        return [name for name in listdir(directory) if name[:1] != "." and name[:2] != "__" and name != "_DS_Store"]
 
-	async def fetch(self, url, raise_on_failure=True, retry=HTTP_RETRY_LIMIT):
-		try:
-			async with self.session.get(url) as response:
-				text = await response.text()
+    @staticmethod
+    def coro_async(corofn, *args):
+        # https://stackoverflow.com/questions/46074841/why-coroutines-cannot-be-used-with-run-in-executor
+        loop = asyncio.new_event_loop()
 
-				if raise_on_failure:
-					if response.status >= 500:
-						if retry != 0:
-							retry -= 1
-							await asyncio.sleep(1.0)
+        try:
+            coro = corofn(*args)
+            asyncio.set_event_loop(loop)
 
-							return await self.fetch(url, raise_on_failure=raise_on_failure, retry=retry)
+            return loop.run_until_complete(coro)
 
-						raise RobloxAPIError
+        finally:
+            loop.close()
 
-					elif response.status == 400:
-						raise RobloxAPIError
-					elif response.status == 404:
-						raise RobloxNotFound
+    async def fetch(self, url, raise_on_failure=True, retry=HTTP_RETRY_LIMIT):
+        try:
+            async with self.session.get(url) as response:
+                text = await response.text()
 
-				if text == "The service is unavailable.":
-					raise RobloxDown
+                if raise_on_failure:
+                    if response.status >= 500:
+                        if retry != 0:
+                            retry -= 1
+                            await asyncio.sleep(1.0)
 
-				return text, response
+                            return await self.fetch(url, raise_on_failure=raise_on_failure, retry=retry)
 
-		except ServerDisconnectedError:
-			if retry != 0:
-				return await self.fetch(url, raise_on_failure=raise_on_failure, retry=retry-1)
-			else:
-				raise ServerDisconnectedError
+                        raise RobloxAPIError
 
-		except ClientOSError:
-			# TODO: raise HttpError with non-roblox URLs
-			raise RobloxAPIError
+                    elif response.status == 400:
+                        raise RobloxAPIError
+                    elif response.status == 404:
+                        raise RobloxNotFound
 
-	async def get_prefix(self, guild=None, guild_data=None, trello_board=None):
-		if not guild:
-			return PREFIX, None
+                if text == "The service is unavailable.":
+                    raise RobloxDown
 
-		if RELEASE == "MAIN" and await guild.fetch_member(469652514501951518):
-			return "!!", None
+                return text, response
 
-		if trello_board:
-			List = await trello_board.get_list(lambda L: L.name == "Bloxlink Settings")
+        except ServerDisconnectedError:
+            if retry != 0:
+                return await self.fetch(url, raise_on_failure=raise_on_failure, retry=retry-1)
+            else:
+                raise ServerDisconnectedError
 
-			if List:
-				card = await List.get_card(lambda c: c.name[:6] == "prefix")
+        except ClientOSError:
+            # TODO: raise HttpError with non-roblox URLs
+            raise RobloxAPIError
 
-				if card:
-					if card.name == "prefix":
-						if card.desc:
-							return card.desc.strip(), card
+    async def get_prefix(self, guild=None, guild_data=None, trello_board=None):
+        if not guild:
+            return PREFIX, None
 
-					else:
-						match = self.option_regex.search(card.name)
+        if RELEASE == "MAIN" and await guild.fetch_member(469652514501951518):
+            return "!!", None
 
-						if match:
-							return match.group(2), card
+        if trello_board:
+            List = await trello_board.get_list(lambda L: L.name == "Bloxlink Settings")
 
+            if List:
+                card = await List.get_card(lambda c: c.name[:6] == "prefix")
 
+                if card:
+                    if card.name == "prefix":
+                        if card.desc:
+                            return card.desc.strip(), card
 
-		guild_data = guild_data or await self.r.db("canary").table("guilds").get(str(guild.id)).run() or {}
-		prefix = guild_data.get("prefix")
+                    else:
+                        match = self.option_regex.search(card.name)
 
-		return prefix or PREFIX, None
+                        if match:
+                            return match.group(2), card
 
 
-	async def validate_guild(self, guild):
-		owner = guild.owner
 
-		if not self.bloxlink_server:
-			return True
+        guild_data = guild_data or await self.r.db("canary").table("guilds").get(str(guild.id)).run() or {}
+        prefix = guild_data.get("prefix")
 
-		profile, _ = await self.is_premium(owner)
-		if profile.features.get("premium"):
-			return True
+        return prefix or PREFIX, None
 
-		try:
-			member = self.bloxlink_server.get_member(owner.id) or await self.bloxlink_server.fetch_member(owner.id)
-		except NotFound:
-			return False
 
-		if member:
-			if find(lambda r: r.name == "3.0 Access", member.roles):
-				return True
+    async def validate_guild(self, guild):
+        owner = guild.owner
 
+        if not self.bloxlink_server:
+            return True
 
-		return False
+        profile, _ = await self.is_premium(owner)
+        if profile.features.get("premium"):
+            return True
 
+        try:
+            member = self.bloxlink_server.get_member(owner.id) or await self.bloxlink_server.fetch_member(owner.id)
+        except NotFound:
+            return False
 
-	async def add_features(self, user, features, *, days=-1, code=None, premium_anywhere=None):
-		user_data = await self.r.table("users").get(str(user.id)).run() or {"id": str(user.id)}
-		user_data_premium = user_data.get("premium") or {}
-		prem_expiry = user_data_premium.get("expiry", 1)
+        if member:
+            if find(lambda r: r.name == "3.0 Access", member.roles):
+                return True
 
-		if days != -1 and days != 0:
-			t = time()
 
-			if prem_expiry and prem_expiry > t:
-				# premium is still active; add time to it
-				days = (days * 86400) + prem_expiry
-			else:
-				# premium expired
-				days = (days * 86400) + t
-		elif days == -1:
-			days = prem_expiry
-		elif days == "-":
-			days = 1
+        return False
 
-		if code:
-			# delete_code()
-			# add code to redeemed
-			pass
 
-		if "pro" in features:
-			user_data_premium["pro"] = days # TODO: convert to -1
+    async def add_features(self, user, features, *, days=-1, code=None, premium_anywhere=None):
+        user_data = await self.r.table("users").get(str(user.id)).run() or {"id": str(user.id)}
+        user_data_premium = user_data.get("premium") or {}
+        prem_expiry = user_data_premium.get("expiry", 1)
 
-		if "premium" in features:
-			user_data_premium["expiry"] = days # TODO: convert to -1
+        if days != -1 and days != 0:
+            t = time()
 
-		if premium_anywhere:
-			user_data["flags"] = user_data.get("flags") or {}
-			user_data["flags"]["premiumAnywhere"] = True
+            if prem_expiry and prem_expiry > t:
+                # premium is still active; add time to it
+                days = (days * 86400) + prem_expiry
+            else:
+                # premium expired
+                days = (days * 86400) + t
+        elif days == -1:
+            days = prem_expiry
+        elif days == "-":
+            days = 1
 
-		if "-" in features:
-			if "premium" in features:
-				user_data_premium["expiry"] = 1
+        if code:
+            # delete_code()
+            # add code to redeemed
+            pass
 
-			if "pro" in features:
-				user_data_premium["pro"] = 1
+        if "pro" in features:
+            user_data_premium["pro"] = days # TODO: convert to -1
 
-			if len(features) == 1:
-				user_data_premium["expiry"] = 1
-				user_data_premium["pro"] = 1
+        if "premium" in features:
+            user_data_premium["expiry"] = days # TODO: convert to -1
 
-		user_data["premium"] = user_data_premium
+        if premium_anywhere:
+            user_data["flags"] = user_data.get("flags") or {}
+            user_data["flags"]["premiumAnywhere"] = True
 
+        if "-" in features:
+            if "premium" in features:
+                user_data_premium["expiry"] = 1
 
-		await self.r.table("users").insert(user_data, conflict="update").run()
+            if "pro" in features:
+                user_data_premium["pro"] = 1
 
+            if len(features) == 1:
+                user_data_premium["expiry"] = 1
+                user_data_premium["pro"] = 1
 
-	async def has_selly_premium(self, author, author_data):
-		premium = author_data.get("premium") or {}
-		expiry = premium.get("expiry", 1)
-		pro_expiry = premium.get("pro", 1)
+        user_data["premium"] = user_data_premium
 
-		t = time()
-		is_p = expiry == 0 or expiry > t
-		days_premium = expiry != 0 and expiry > t and ceil((expiry - t)/86400) or 0
 
-		pro_access = pro_expiry == 0 or pro_expiry > t
-		pro_days = pro_expiry != 0 and pro_expiry > t and ceil((pro_expiry - t)/86400) or 0
+        await self.r.table("users").insert(user_data, conflict="update").run()
 
-		return {
-			"premium": is_p,
-			"days": days_premium,
-			"pro_access": pro_access,
-			"pro_days": pro_days,
-			"codes_redeemed": premium.get("redeemed", {})
-		}
 
+    async def has_selly_premium(self, author, author_data):
+        premium = author_data.get("premium") or {}
+        expiry = premium.get("expiry", 1)
+        pro_expiry = premium.get("pro", 1)
 
-	async def has_patreon_premium(self, author, author_data):
-		patron_data = await is_patron(author)
+        t = time()
+        is_p = expiry == 0 or expiry > t
+        days_premium = expiry != 0 and expiry > t and ceil((expiry - t)/86400) or 0
 
-		return patron_data
+        pro_access = pro_expiry == 0 or pro_expiry > t
+        pro_days = pro_expiry != 0 and pro_expiry > t and ceil((pro_expiry - t)/86400) or 0
 
+        return {
+            "premium": is_p,
+            "days": days_premium,
+            "pro_access": pro_access,
+            "pro_days": pro_days,
+            "codes_redeemed": premium.get("redeemed", {})
+        }
 
-	async def transfer_premium(self, transfer_from, transfer_to):
-		profile, _ = await self.is_premium(transfer_to)
-		if profile.features.get("premium"):
-			raise Message("This user already has premium!", type="silly")
 
-		if transfer_from == transfer_to:
-			raise Message("You cannot transfer premium to yourself!")
+    async def has_patreon_premium(self, author, author_data):
+        patron_data = await is_patron(author)
 
+        return patron_data
 
-		transfer_from_data = await self.r.table("users").get(str(transfer_from.id)).run() or {"id": str(transfer_from.id)}
-		transfer_to_data   = await self.r.table("users").get(str(transfer_to.id)).run() or {"id": str(transfer_to.id)}
 
-		transfer_from_data["premium"] = transfer_from_data.get("premium", {})
-		transfer_to_data["premium"]   = transfer_to_data.get("premium", {})
+    async def transfer_premium(self, transfer_from, transfer_to):
+        profile, _ = await self.is_premium(transfer_to)
+        if profile.features.get("premium"):
+            raise Message("This user already has premium!", type="silly")
 
-		transfer_from_data["premium"]["transferTo"] = str(transfer_to.id)
-		transfer_to_data["premium"]["transferFrom"] = str(transfer_from.id)
+        if transfer_from == transfer_to:
+            raise Message("You cannot transfer premium to yourself!")
 
-		await self.r.table("users").insert(transfer_from_data, conflict="update").run()
-		await self.r.table("users").insert(transfer_to_data,   conflict="update").run()
 
+        transfer_from_data = await self.r.table("users").get(str(transfer_from.id)).run() or {"id": str(transfer_from.id)}
+        transfer_to_data   = await self.r.table("users").get(str(transfer_to.id)).run() or {"id": str(transfer_to.id)}
 
-	async def is_premium(self, author, author_data=None, rec=True):
-		profile = DonatorProfile(author)
+        transfer_from_data["premium"] = transfer_from_data.get("premium", {})
+        transfer_to_data["premium"]   = transfer_to_data.get("premium", {})
 
-		author_data = author_data or await self.r.table("users").get(str(author.id)).run() or {"id": str(author.id)}
-		premium_data = author_data.get("premium") or {}
+        transfer_from_data["premium"]["transferTo"] = str(transfer_to.id)
+        transfer_to_data["premium"]["transferFrom"] = str(transfer_from.id)
 
-		if rec:
-			if premium_data.get("transferTo"):
-				return profile, premium_data["transferTo"]
-			elif premium_data.get("transferFrom"):
-				transfer_from = premium_data["transferFrom"]
-				transferee_data = await self.r.table("users").get(str(transfer_from)).run() or {}
-				transferee_premium, _ = await self.is_premium(Object(id=transfer_from), transferee_data, rec=False)
+        await self.r.table("users").insert(transfer_from_data, conflict="update").run()
+        await self.r.table("users").insert(transfer_to_data,   conflict="update").run()
 
-				if transferee_premium:
-					return transferee_premium, _
-				else:
-					premium_data["transferFrom"] = None
-					transferee_data["transferTo"] = None
 
-					author_data["premium"] = premium_data
-					transferee_data["premium"] = transferee_data
+    async def is_premium(self, author, author_data=None, rec=True):
+        profile = DonatorProfile(author)
 
-					await self.r.table("users").insert(author_data, conflict="update").run()
-					await self.r.table("users").insert(transferee_data, conflict="update").run()
+        author_data = author_data or await self.r.table("users").get(str(author.id)).run() or {"id": str(author.id)}
+        premium_data = author_data.get("premium") or {}
 
+        if rec:
+            if premium_data.get("transferTo"):
+                return profile, premium_data["transferTo"]
+            elif premium_data.get("transferFrom"):
+                transfer_from = premium_data["transferFrom"]
+                transferee_data = await self.r.table("users").get(str(transfer_from)).run() or {}
+                transferee_premium, _ = await self.is_premium(Object(id=transfer_from), transferee_data, rec=False)
 
-		if author_data.get("flags", {}).get("premiumAnywhere"):
-			profile.attributes["PREMIUM_ANYWHERE"] = True
+                if transferee_premium:
+                    return transferee_premium, _
+                else:
+                    premium_data["transferFrom"] = None
+                    transferee_data["transferTo"] = None
 
-		data_patreon = await self.has_patreon_premium(author, author_data)
+                    author_data["premium"] = premium_data
+                    transferee_data["premium"] = transferee_data
 
-		if data_patreon:
-			profile.load_patreon(data_patreon)
-			profile.add_features("premium", "pro")
-		else:
-			data_selly = await self.has_selly_premium(author, author_data)
+                    await self.r.table("users").insert(author_data, conflict="update").run()
+                    await self.r.table("users").insert(transferee_data, conflict="update").run()
 
-			if data_selly["premium"]:
-				profile.add_features("premium")
-				profile.load_selly(days=data_selly["days"])
 
-			if data_selly["pro_access"]:
-				profile.add_features("pro")
+        if author_data.get("flags", {}).get("premiumAnywhere"):
+            profile.attributes["PREMIUM_ANYWHERE"] = True
 
+        data_patreon = await self.has_patreon_premium(author, author_data)
 
-		return profile, None
+        if data_patreon:
+            profile.load_patreon(data_patreon)
+            profile.add_features("premium", "pro")
+        else:
+            data_selly = await self.has_selly_premium(author, author_data)
+
+            if data_selly["premium"]:
+                profile.add_features("premium")
+                profile.load_selly(days=data_selly["days"])
+
+            if data_selly["pro_access"]:
+                profile.add_features("pro")
+
+
+        return profile, None
