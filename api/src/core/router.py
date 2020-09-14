046241a0ec0e16167419cc51eb3b350a4e98d9a9
tigerism <tigerism@users.noreply.github.com>
2020-09-14 17:12:25 -0400

    Remove old API

--- api/src/core/router.py
+++
@@ -1,9 +0,0 @@
-from controllers import controller
-
-def routes(app):
-    app.router.add_get('/', controller.index)
-    app.router.add_get('/v1/user/{id}', controller.user)
-    app.router.add_get('/v1/game/{roblox_id}', controller.game_get)
-    app.router.add_delete('/v1/game/{roblox_id}', controller.game_delete)
-    app.router.add_post('/v1/verify/{discord_id}/{roblox_id}', controller.user_verify)
-    app.router.add_get('/v1/commands/', controller.list_commands)
