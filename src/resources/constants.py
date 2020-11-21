ca729d8139c16182f3f7fcbca05d925a1db7434c
xomien <xomien@users.noreply.github.com>
2020-11-21 01:52:23 -0500

    Version bump to 3.13

--- src/resources/constants.py
+++ src/resources/constants.py
@@ -4,7 +4,7 @@ from re import search
 from discord import Game
 from discord.utils import find
 
-VERSION = "v3.12"
+VERSION = "v3.13"
 
 RELEASE = env.get("RELEASE", "LOCAL")
 IS_DOCKER = bool(env.get("RELEASE"))
