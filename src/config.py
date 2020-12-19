f3d5dcd81308215b380932593339b41fdb13ec4a
xomien <xomien01@gmail.com>
2020-12-19 15:31:42 -0500

    Simplify the config.py example file

--- src/config.py
+++ src/config.py
@@ -1,12 +1,6 @@
 from resources.constants import RELEASE, IS_DOCKER # pylint: disable=import-error
 
-
-MODULE_DIR = [ # load these modules
-	"src/resources/modules",
-	"src/resources/events",
-	"src/commands"
-]
-
+# More optional settings can be set, such as a Trello key/token. Look at the docker-compose.yml for additional values.
 
 PREFIX = "!"
 
@@ -14,40 +8,6 @@ WEBHOOKS = { # discord webhook links
 	"LOGS":  None,
 	"ERRORS": None
 }
-
-PROMPT = {
-	"PROMPT_TIMEOUT": 300,
-	"PROMPT_ERROR_COUNT": 5
-}
-
-HTTP_RETRY_LIMIT = 5
-
-WORDS = [
-	"bus",
-	"roblox",
-	"book",
-	"key",
-	"shirt",
-	"pants",
-	"battery",
-	"lamp",
-	"desk",
-	"water",
-	"soda",
-	"button",
-	"can",
-	"hello",
-	"mouse",
-	"vase",
-	"rug",
-	"blanket",
-	"pillow",
-	"music",
-	"lego",
-	"glasses",
-	"controller",
-	"pencil"
-]
 
 REACTIONS = { # discord emote mention strings
 	"LOADING": "<a:BloxlinkLoading:530113171734921227>",
@@ -58,18 +18,15 @@ REACTIONS = { # discord emote mention strings
 	"BANNED": "<:ban:476838302092230672>"
 }
 
-VERIFYALL_MAX_SCAN = 5 # max concurrent !verifyall scans
+RETHINKDB_HOST = "rethinkdb"
+RETHINKDB_PASSWORD = None
+RETHINKDB_PORT = 28015
+RETHINKDB_DB = "bloxlink"
 
-if RELEASE == "LOCAL" and not IS_DOCKER: # needed for easy local tests without spawning Docker
-	RETHINKDB_HOST = "rethinkdb"
-	RETHINKDB_PASSWORD = None
-	RETHINKDB_PORT = 28015
-	RETHINKDB_DB = "bloxlink"
+REDIS_HOST = "redis"
+REDIS_PORT = 6379
+REDIS_PASSWORD = None
 
-	REDIS_HOST = "redis"
-	REDIS_PORT = 6379
-	REDIS_PASSWORD = None
-
-	TOKEN = None
+TOKEN = None
 
 BLOXLINK_GUILD = None # your guild ID, used to load nitro boosters and other data
