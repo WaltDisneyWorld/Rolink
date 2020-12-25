0533c487f028c987bf75c65cc14e2191ff94a346
xomien <xomien01@gmail.com>
2020-12-25 16:21:01 -0500

    Showing loading reaction from !verifychannel

--- src/commands/verifychannel.py
+++ src/commands/verifychannel.py
@@ -20,65 +20,67 @@ class VerifyChannelCommand(Bloxlink.Module):
 
         guild = CommandArgs.message.guild
 
-        try:
-            category = find(lambda c: c.name == "Verification", guild.categories) or \
-                       await guild.create_category("Verification")
+        async with response.loading():
 
-            verify_info = find(lambda t: t.name == "verify-instructions", category.channels) or \
-                          await guild.create_text_channel("verify-instructions", category=category)
+            try:
+                category = find(lambda c: c.name == "Verification", guild.categories) or \
+                        await guild.create_category("Verification")
 
-            verify_channel = find(lambda t: t.name == "verify", category.channels) or \
-                             await guild.create_text_channel("verify", category=category)
+                verify_info = find(lambda t: t.name == "verify-instructions", category.channels) or \
+                            await guild.create_text_channel("verify-instructions", category=category)
 
-            sample_channel = await guild.create_text_channel("sample-channel")
+                verify_channel = find(lambda t: t.name == "verify", category.channels) or \
+                                await guild.create_text_channel("verify", category=category)
 
-        except Forbidden:
-            raise PermissionError("I was unable to create the necessary channels. Please ensure I have the "
-                                  "``Manage Channels`` permission.")
+                sample_channel = await guild.create_text_channel("sample-channel")
 
-        except HTTPException:
-            raise Error("You have too many channels or categories! Please delete some before continuing.")
+            except Forbidden:
+                raise PermissionError("I was unable to create the necessary channels. Please ensure I have the "
+                                    "``Manage Channels`` permission.")
 
-        try:
-            await verify_info.send("This server uses Bloxlink to manage Roblox verification. In "
-                                   "order to unlock all the features of this server, you'll need "
-                                   "to verify your Roblox account with your Discord account!\n\nTo "
-                                   f"do this, run ``{prefix}verify`` in {verify_channel.mention} and follow the instructions.")
+            except HTTPException:
+                raise Error("You have too many channels or categories! Please delete some before continuing.")
 
-            await sample_channel.send("This is a sample channel that only Verified users " \
-                                      "can read. This channel is not important, you may freely delete it.\n" \
-                                      "To create another sample channel, right click this channel and click 'Clone " \
-                                      "Text Channel', or just run this command again.")
+            try:
+                await verify_info.send("This server uses Bloxlink to manage Roblox verification. In "
+                                    "order to unlock all the features of this server, you'll need "
+                                    "to verify your Roblox account with your Discord account!\n\nTo "
+                                    f"do this, run ``{prefix}verify`` in {verify_channel.mention} and follow the instructions.")
 
-        except (Forbidden, NotFound):
-            raise PermissionError("I was unable to send messages to the created channels. Please give me the "
-                                  "proper permissions.")
+                await sample_channel.send("This is a sample channel that only Verified users " \
+                                        "can read. This channel is not important, you may freely delete it.\n" \
+                                        "To create another sample channel, right click this channel and click 'Clone " \
+                                        "Text Channel', or just run this command again.")
 
-
-        try:
-            await verify_info.set_permissions(guild.me, send_messages=True, read_messages=True)
-
-            verified_role_name = guild_data.get("verifiedRoleName", DEFAULTS.get("verifiedRoleName"))
-
-            for role in guild.roles:
-                if role.name != guild.me.name:
-                    await verify_info.set_permissions(role, send_messages=False, read_messages=True)
-                    await verify_channel.set_permissions(role, send_messages=True, read_messages=True)
-
-                if role.name == verified_role_name:
-                    for target, overwrite in sample_channel.overwrites.items():
-                        await sample_channel.set_permissions(target, overwrite=None)
-
-                    await sample_channel.set_permissions(guild.default_role, send_messages=False, read_messages=False)
-                    await sample_channel.set_permissions(role, send_messages=True, read_messages=True)
+            except (Forbidden, NotFound):
+                raise PermissionError("I was unable to send messages to the created channels. Please give me the "
+                                    "proper permissions.")
 
 
-        except Forbidden:
-            raise PermissionError("Unable to set permissions to the channels. Please ensure I have the "
-                                  "``Manage Channels`` and ``Manage Roles`` permission.")
+            try:
+                await verify_info.set_permissions(guild.me, send_messages=True, read_messages=True)
 
-        except NotFound:
-            raise Error("Please do not delete the created channels while I'm setting them up...")
+                verified_role_name = guild_data.get("verifiedRoleName", DEFAULTS.get("verifiedRoleName"))
+
+                for role in guild.roles:
+                    if role.name != guild.me.name:
+                        await verify_info.set_permissions(role, send_messages=False, read_messages=True)
+                        await verify_channel.set_permissions(role, send_messages=True, read_messages=True)
+
+                    if role.name == verified_role_name:
+                        for target, overwrite in sample_channel.overwrites.items():
+                            await sample_channel.set_permissions(target, overwrite=None)
+
+                        await sample_channel.set_permissions(guild.default_role, send_messages=False, read_messages=False)
+                        await sample_channel.set_permissions(role, send_messages=True, read_messages=True)
+
+
+            except Forbidden:
+                raise PermissionError("Unable to set permissions to the channels. Please ensure I have the "
+                                    "``Manage Channels`` and ``Manage Roles`` permission.")
+
+            except NotFound:
+                raise Error("Please do not delete the created channels while I'm setting them up...")
 
 
         await self.r.table("guilds").insert({
