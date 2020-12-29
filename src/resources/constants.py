7c01f39b725c456b0b738d9e4ff35112e0ca97f1
xomien <xomien01@gmail.com>
2020-12-28 22:00:04 -0500

    Version bump to 3.14.4

--- src/resources/constants.py
+++ src/resources/constants.py
@@ -4,7 +4,7 @@ from re import search
 from discord import Game
 from discord.utils import find
 
-VERSION = "v3.14.3"
+VERSION = "v3.14.4"
 
 RELEASE = env.get("RELEASE", "LOCAL")
 IS_DOCKER = bool(env.get("RELEASE"))
