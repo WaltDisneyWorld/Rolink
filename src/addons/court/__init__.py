d39b20c6f0b5fe9aba7a070f4d39a452e48cfb41
xomien <xomien01@gmail.com>
2021-01-02 04:02:38 -0500

    Court add-on category

---
+++ src/addons/court/__init__.py
@@ -0,0 +1,14 @@
+from resources.constants import ARROW # pylint: disable=import-error
+
+class CourtAddon:
+    """provides judicial commands"""
+
+    def __init__(self):
+        self.default_enabled = False
+        self.toggleable = True
+
+    def __str__(self):
+        return self.__class__.__name__.replace("Addon", "").lower()
+
+    def __repr__(self):
+        return f"**{self.__str__()}** {ARROW} {self.__doc__}"
