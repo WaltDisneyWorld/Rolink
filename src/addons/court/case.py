ce9ab84d7a23077cf25a6aaaaf0c833dabc0aaa6
xomien <xomien01@gmail.com>
2021-01-03 02:15:06 -0500

    Implement court commands

--- src/addons/court/case.py
+++ src/addons/court/case.py
@@ -1,22 +1,35 @@
 from resources.structures.Bloxlink import Bloxlink # pylint: disable=import-error
 from resources.exceptions import Error, Message # pylint: disable=import-error
-from discord import PermissionOverwrite
+from resources.constants import ARROW # pylint: disable=import-error
+from discord import PermissionOverwrite, Embed, AllowedMentions
 from discord.utils import find
-from discord.errors import Forbidden
+from discord.errors import Forbidden, NotFound
 
 
 class CaseCommand(Bloxlink.Module):
     """manage your cases"""
 
     def __init__(self):
-        self.developer = True
         self.permissions = Bloxlink.Permissions().build("BLOXLINK_MANAGER")
         self.category = "Court Addon"
 
     async def __main__(self, CommandArgs):
-        prefix   = CommandArgs.prefix
-        guild    = CommandArgs.message.guild
-        response = CommandArgs.response
+        subcommands = [getattr(self, x) for x in dir(self) if hasattr(getattr(self, x),"__issubcommand__")]
+        subcommands_str = "\n".join(["**" + x.__func__.__name__ + "**" + f" {ARROW} " + x.__doc__ for x in subcommands])
+
+        subcommand_name = (await CommandArgs.prompt([
+            {
+                "prompt": "Please choose a subcommand:\n\n"
+                          f"{subcommands_str}",
+                "name": "subcommand",
+                "type": "choice",
+                "choices": [x.__func__.__name__ for x in subcommands],
+                "formatting": False
+            }
+        ]))["subcommand"]
+
+        subcommand = getattr(self, subcommand_name)
+        await subcommand(CommandArgs)
 
 
     @Bloxlink.subcommand()
@@ -33,8 +46,8 @@ class CaseCommand(Bloxlink.Module):
 
         my_permissions = guild.me.guild_permissions
 
-        if not my_permissions.manage_channels:
-            raise Error("I need permission to create channels!")
+        if not (my_permissions.manage_channels and my_permissions.manage_roles):
+            raise Error("I need both the ``Manage Channels`` and ``Manage Roles`` permissions.")
 
         if not court_data:
             raise Error(f"You must set-up this add-on before you can use it! Please use ``{prefix}courtsetup`` "
@@ -44,7 +57,12 @@ class CaseCommand(Bloxlink.Module):
             if find(lambda r: r.id == int(judge_role_id), guild.roles):
                 break
         else:
-            raise Error("You must have a Judge role in order to create cases!") # TODO: post role mentions with AllowedMentions set
+            raise Error("You must have a Judge role in order to create cases!")
+
+        if len(court_data.get("cases", {})) >= 30:
+            raise Error(f"You cannot have more than 30 active cases! Please complete the cases with ``{prefix}case end`` before creating more. "
+                        f"If you have any lingering cases (cases manually deleted and not called with ``{prefix}case end``), then please run "
+                        f"``{prefix}case cleanup`` to remove them from the database.")
 
 
         case_args = await CommandArgs.prompt([
@@ -85,7 +103,7 @@ class CaseCommand(Bloxlink.Module):
         try:
             case_channel = await guild.create_text_channel(case_name.replace("_", "").replace(" ", "-"), overwrites=overwrites, category=category)
         except Forbidden:
-            pass
+            raise Error("I need both the ``Manage Channels`` and ``Manage Roles`` permissions.")
         else:
             court_data["cases"] = court_data.get("cases", {})
             court_data["cases"][str(case_channel.id)] = court_data["cases"].get(str(case_channel.id)) or {}
@@ -105,12 +123,28 @@ class CaseCommand(Bloxlink.Module):
             await response.success(f"Your case chat was successfully created! You'll find it in {case_channel.mention}. If your DMs are enabled, then I'll send you "
                                     "additional instructions over DMs; otherwise, I'll post it in this channel.")
 
-            await response.send(f"Judge: run ``{prefix}case end`` to end this case and export the chat logs. Run ``{prefix}case add @person1 @person2`` "
-                                f"to add additional people to the case. Run ``{prefix}jurors <integer>`` to randomly pick <int> jurors. Finally, run "
-                                f"``{prefix}case mute <plaintiff|defense>`` to mute those people, and ``{prefix}case unmute <plaintiff|defense>`` to unmute them.", dm=True) # TODO: make sure this is up-to-date
+            embed = Embed(title="Additional Information", description= \
+                                                                  f"- It's recommended to give your cases IDs! You'll be able to run ``{prefix}case lookup`` to find its information.\n"
+                                                                  f"- You will need to add case members with ``{prefix}case add`` and assign them to a group. Remove them with ``{prefix}case remove``.\n"
+                                                                  f"- Run ``{prefix}case end`` inside your case chat to archive the case.\n"
+                                                                  f"- If you manually delete case channels, then you will need to run ``{prefix}case cleanup`` to free them from the database; otherwise, they will count against your case limit.\n"
+                                                                  f"- Case members can be easily muted/unmuted with ``{prefix}case mute`` or ``{prefix}case unmute``.")
+
+            log_channel_id = court_data.get("logChannel")
+
+            if log_channel_id:
+                log_channel = guild.get_channel(int(log_channel_id))
+
+                if log_channel:
+                    await response.send(f"Case **{case_name}** has been opened by {author.mention} and being served in {case_channel.mention}.", channel_override=log_channel, allowed_mentions=AllowedMentions(users=False))
+
+            await response.send(embed=embed)
+
 
     @Bloxlink.subcommand()
     async def add(self, CommandArgs):
+        """add members to court groups"""
+
         response = CommandArgs.response
 
         guild   = CommandArgs.message.guild
@@ -171,8 +205,11 @@ class CaseCommand(Bloxlink.Module):
 
         await response.success(f"Successfully **added** these members to the group ``{group}``!")
 
+
     @Bloxlink.subcommand()
     async def remove(self, CommandArgs):
+        """remove members from court groups"""
+
         response = CommandArgs.response
         prefix   = CommandArgs.prefix
 
@@ -233,3 +270,286 @@ class CaseCommand(Bloxlink.Module):
                         "and ``Manage Roles`` permissions, then re-run this command.")
 
         await response.success(f"Successfully **removed** these members from the group ``{group}``!")
+
+
+    @Bloxlink.subcommand()
+    async def end(self, CommandArgs):
+        """end your trial"""
+
+        response = CommandArgs.response
+        prefix   = CommandArgs.prefix
+
+        guild   = CommandArgs.message.guild
+        channel = CommandArgs.message.channel
+        author  = CommandArgs.message.author
+
+        addon_data = await self.r.db("bloxlink").table("addonData").get(str(guild.id)).run() or {"id": str(guild.id)}
+        court_data = addon_data.get("court") or {}
+        cases = court_data.get("cases", {})
+
+        archive_category = court_data.get("archiveCategory")
+
+        current_case = cases.get(str(channel.id)) or {}
+        case_name    = current_case.get("caseName")
+
+        if not current_case:
+            raise Error("You must run this command in a case channel!")
+
+        elif int(current_case["presidingJudge"]) != author.id:
+            raise Error("You must be the presiding judge in order to run this command!")
+
+        if archive_category:
+            category = find(lambda c: c.id == int(archive_category), guild.categories)
+
+            if not category:
+                raise Error(f"The archive category has been deleted! You must run ``{prefix}courtsetup`` to set another category.")
+
+            overwrites = {
+                guild.default_role: PermissionOverwrite(read_messages=False)
+            }
+
+            try:
+                await channel.edit(category=category, overwrites=overwrites)
+            except NotFound:
+                raise Error(f"The archive category has been deleted! You must run ``{prefix}courtsetup`` to set another category.")
+            except Forbidden:
+                raise Error("I was unable to edit this case channel. Please make sure I have both the ``Manage Channels`` and "
+                            "``Manage Roles`` permissions.")
+
+            else:
+                await response.success("Successfully **closed** this case channel.")
+        else:
+            try:
+                await channel.delete()
+            except Forbidden:
+                raise Error("I was unable to delete this channel. Please make sure I have the ``Manage Channels`` permission.")
+
+        cases.pop(str(channel.id), None)
+        court_data["cases"] = cases
+        addon_data["court"] = court_data
+
+        await self.r.table("addonData").insert(addon_data, conflict="replace").run()
+
+        log_channel_id = court_data.get("logChannel")
+
+        if log_channel_id:
+            log_channel = guild.get_channel(int(log_channel_id))
+
+            if log_channel:
+                await response.send(f"Case **{case_name}** has been closed by {author.mention}.", channel_override=log_channel, allowed_mentions=AllowedMentions(users=False))
+
+
+    @Bloxlink.subcommand(arguments=[{
+        "prompt": "Please provide a Case ID.",
+        "name": "case_id"
+    }])
+    async def lookup(self, CommandArgs):
+        """lookup a case by its ID"""
+
+        case_id = CommandArgs.parsed_args["case_id"]
+
+        response = CommandArgs.response
+        prefix   = CommandArgs.prefix
+
+        guild   = CommandArgs.message.guild
+
+        addon_data = await self.r.db("bloxlink").table("addonData").get(str(guild.id)).run() or {"id": str(guild.id)}
+        court_data = addon_data.get("court") or {}
+        cases = court_data.get("cases", {})
+
+
+        if not court_data:
+            raise Error(f"You must set-up this add-on before you can use it! Please use ``{prefix}courtsetup`` "
+                        "to begin the set-up.")
+        elif not cases:
+            raise Error(f"This server has no active cases! Cases may be created with ``{prefix}case create``.")
+
+
+        for channel_id, case in cases.items():
+            if case["caseID"] == case_id:
+                break
+        else:
+            raise Error("I was unable to find the selected case! It may have already been closed.")
+
+        group_members_ = case.get("groupMembers", {})
+        group_members_str = "None"
+
+        if group_members_:
+            group_members_str = []
+
+            for group, group_members in group_members_.items():
+                group_members_str.append(f"**{group}** {ARROW} {', '.join([f'<@{m}>' for m in group_members])}")
+
+            group_members_str = "\n".join(group_members_str)
+
+
+        embed = Embed(title=f"Case {case['caseName']}")
+
+        embed.add_field(name="Case ID", value=case_id)
+        embed.add_field(name="Presiding Judge", value=f"<@{case['presidingJudge']}>")
+        embed.add_field(name="Case Channel", value=f"<#{channel_id}>")
+        embed.add_field(name="Group Members", value=group_members_str, inline=False)
+
+        await response.send(embed=embed)
+
+
+    @Bloxlink.subcommand()
+    async def cleanup(self, CommandArgs):
+        """free old cases from the database"""
+
+        guild    = CommandArgs.message.guild
+        prefix   = CommandArgs.prefix
+        response = CommandArgs.response
+
+        addon_data = await self.r.db("bloxlink").table("addonData").get(str(guild.id)).run() or {"id": str(guild.id)}
+        court_data = addon_data.get("court")
+        cases = court_data.get("cases", {})
+
+        removed = 0
+
+        my_permissions = guild.me.guild_permissions
+
+        if not (my_permissions.manage_channels and my_permissions.manage_roles):
+            raise Error("I need both the ``Manage Channels`` and ``Manage Roles`` permissions.")
+
+        elif not court_data:
+            raise Error(f"You must set-up this add-on before you can use it! Please use ``{prefix}courtsetup`` "
+                        "to begin the set-up.")
+
+        elif not cases:
+            raise Message("Cannot clean cases: you have no cases saved to the database.", type="silly")
+
+
+        for judge_role_id in court_data.get("judgeRoles", []):
+            if find(lambda r: r.id == int(judge_role_id), guild.roles):
+                break
+        else:
+            raise Error("You must have a Judge role in order to run this command!")
+
+        for channel_id, case in dict(cases).items():
+            case_channel = guild.get_channel(int(channel_id))
+
+            if not case_channel or (case_channel and case_channel.category and case_channel.category.id != case["archiveCategory"]):
+                cases.pop(channel_id)
+                removed += 1
+
+
+        court_data["cases"] = cases
+        addon_data["court"] = court_data
+
+        await self.r.table("addonData").insert(addon_data, conflict="replace").run()
+
+        if removed:
+            await response.success(f"Successfully removed **{removed}** old case(s) from the database.")
+        else:
+            await response.silly("No cases to clean: all case channels still exist in your server.")
+
+
+    @Bloxlink.subcommand()
+    async def mute(self, CommandArgs):
+        """mute case members"""
+
+        response = CommandArgs.response
+        prefix   = CommandArgs.prefix
+
+        guild   = CommandArgs.message.guild
+        channel = CommandArgs.message.channel
+        author  = CommandArgs.message.author
+
+        addon_data = await self.r.db("bloxlink").table("addonData").get(str(guild.id)).run() or {"id": str(guild.id)}
+        court_data = addon_data.get("court") or {}
+        groups     = court_data.get("groups", [])
+
+        current_case = court_data.get("cases", {}).get(str(channel.id))
+
+        if not current_case:
+            raise Error("You must run this command in a case channel!")
+
+        elif int(current_case["presidingJudge"]) != author.id:
+            raise Error("You must be the presiding judge in order to run this command!")
+
+        elif not groups:
+            raise Error("You need at least one group in order to run this command!") # TODO: add instructions on creating groups
+
+        parsed_args = await CommandArgs.prompt([
+            {
+                "prompt": f"Which group should be muted? Available groups: ``{groups}``",
+                "name": "group",
+                "type": "choice",
+                "choices": groups,
+                "formatting": False
+            }
+        ])
+
+        group = parsed_args["group"]
+        group_members = current_case["groupMembers"].get(group) or []
+
+        if not group_members:
+            raise Error(f"This group has no members associated with it! Please add them with ``{prefix}case add``")
+
+        if not guild.chunked:
+            await guild.chunk()
+
+        for member_id in group_members:
+            member = guild.get_member(int(member_id))
+
+            if member:
+                await channel.set_permissions(member, send_messages=False)
+
+
+        await response.success(f"Successfully **muted** the members from group ``{group}``!")
+
+
+    @Bloxlink.subcommand()
+    async def unmute(self, CommandArgs):
+        """unmute case members"""
+
+        response = CommandArgs.response
+        prefix   = CommandArgs.prefix
+
+        guild   = CommandArgs.message.guild
+        channel = CommandArgs.message.channel
+        author  = CommandArgs.message.author
+
+        addon_data = await self.r.db("bloxlink").table("addonData").get(str(guild.id)).run() or {"id": str(guild.id)}
+        court_data = addon_data.get("court") or {}
+        groups     = court_data.get("groups", [])
+
+        current_case = court_data.get("cases", {}).get(str(channel.id))
+
+        if not current_case:
+            raise Error("You must run this command in a case channel!")
+
+        elif int(current_case["presidingJudge"]) != author.id:
+            raise Error("You must be the presiding judge in order to run this command!")
+
+        elif not groups:
+            raise Error("You need at least one group in order to run this command!") # TODO: add instructions on creating groups
+
+        parsed_args = await CommandArgs.prompt([
+            {
+                "prompt": f"Which group should be muted? Available groups: ``{groups}``",
+                "name": "group",
+                "type": "choice",
+                "choices": groups,
+                "formatting": False
+            }
+        ])
+
+        group = parsed_args["group"]
+        group_members = current_case["groupMembers"].get(group) or []
+
+        if not group_members:
+            raise Error(f"This group has no members associated with it! Please add them with ``{prefix}case add``")
+
+        if not guild.chunked:
+            await guild.chunk()
+
+        for member_id in group_members:
+            member = guild.get_member(int(member_id))
+
+            if member:
+                await channel.set_permissions(member, overwrite=None)
+
+
+        await response.success(f"Successfully **unmuted** the members from group ``{group}``!")
--- src/addons/court/courtsetup.py
+++ src/addons/court/courtsetup.py
@@ -2,14 +2,13 @@ from resources.structures.Bloxlink import Bloxlink # pylint: disable=import-erro
 from resources.exceptions import Error # pylint: disable=import-error
 from discord.utils import find
 from discord.errors import Forbidden
-from discord import Embed
+from discord import Embed, PermissionOverwrite, AllowedMentions
 
 
 class CourtSetupCommand(Bloxlink.Module):
     """set-up your judicial system"""
 
     def __init__(self):
-        self.developer = True
         self.permissions = Bloxlink.Permissions().build("BLOXLINK_MANAGER")
         self.category = "Court Addon"
 
@@ -20,8 +19,8 @@ class CourtSetupCommand(Bloxlink.Module):
 
         my_permissions = guild.me.guild_permissions
 
-        if not my_permissions.manage_channels:
-            raise Error("I need permission to create channels!")
+        if not (my_permissions.manage_channels and my_permissions.manage_roles):
+            raise Error("I need both the ``Manage Channels`` and ``Manage Roles`` permissions.")
 
         setup_args = await CommandArgs.prompt([
             {
@@ -51,6 +50,14 @@ class CourtSetupCommand(Bloxlink.Module):
                 "type": "channel",
             },
             {
+                "prompt": "Which ``category`` would you like to use for archiving closed cases? If you skip this, then old cases "
+                          "will simply be deleted.",
+                "name": "archive_category",
+                "footer": "Say **skip** to skip this step.",
+                "exceptions": ("skip",),
+                "type": "category",
+            },
+            {
                 "prompt": "Please specify the **groups** to use with your set-up.\n\nA group holds members for each trial. For example, typical groups "
                           "for a USA style court room include the ``prosecution``, ``defence``, ``jurors``, and ``witnesses``. Please **give a list separated by "
                           "commas** of your group names.\n\nFor example, you may write: ``prosecution, defense, jurors``\n\nJudges will be able to add people into these groups to fit their trial.\n\n__It's fine if you include groups "
@@ -60,26 +67,56 @@ class CourtSetupCommand(Bloxlink.Module):
             }
         ]) # TODO: ask for Trello information
 
+        judge_roles     = setup_args["judge_roles"]
+        log_channel     = setup_args["log_channel"]
+        groups          = setup_args["groups"]
+        archive_category = setup_args["archive_category"]
+
         addon_data = await self.r.db("bloxlink").table("addonData").get(str(guild.id)).run() or {}
         court_data = addon_data.get("court") or {}
 
         category = None
+        create_category = False
 
-        if not court_data.get("category"):
+        if archive_category != "skip":
+            overwrites = {
+                guild.default_role: PermissionOverwrite(read_messages=False),
+                guild.me:           PermissionOverwrite(read_messages=True),
+            }
+            overwrites.update({k:PermissionOverwrite(read_messages=True) for k in judge_roles})
+
+            try:
+                await archive_category.edit(overwrites=overwrites)
+            except Forbidden:
+                raise Error("I need both the ``Manage Channels`` and ``Manage Roles`` permissions.")
+
+        if court_data.get("category"):
+            category = find(lambda c: c.id == int(court_data["category"]), guild.categories)
+
+            if not category:
+                create_category = True
+        else:
+            create_category = True
+
+        if create_category:
+            overwrites = {
+                guild.default_role: PermissionOverwrite(read_messages=False),
+                guild.me:           PermissionOverwrite(read_messages=True),
+            }
+            overwrites.update({k:PermissionOverwrite(read_messages=True) for k in judge_roles})
+
             try:
                 category = find(lambda c: c.name == "Cases", guild.categories) or \
-                           await guild.create_category("Cases")
-            except Forbidden:
-                raise Error("I need permission to create channels!")
+                           await guild.create_category("Cases", overwrites=overwrites)
 
-        judge_roles = setup_args["judge_roles"]
-        log_channel = setup_args["log_channel"]
-        groups      = setup_args["groups"]
+            except Forbidden:
+                raise Error("I need both the ``Manage Channels`` and ``Manage Roles`` permissions.")
 
         addon_data["court"] = {
             "judgeRoles": [str(x.id) for x in judge_roles],
-            "logChannel": log_channel != "skip" and str(log_channel.id),
-            "category": category and str(category.id) or addon_data.get("court", {}).get("category"),
+            "logChannel": str(log_channel.id) if log_channel != "skip" else None,
+            "archiveCategory": str(archive_category.id) if archive_category != "skip" else None,
+            "category": str(category.id) if category else None,
             "groups": groups
         }
 
@@ -89,9 +126,15 @@ class CourtSetupCommand(Bloxlink.Module):
         }, conflict="update").run()
 
 
-        #embed = Embed(title="Additional Information") TODO: post info with all the available commands
+        embed = Embed(title="Additional Information", description=f"- Use ``{prefix}case create`` to make a new case chat.\n"
+                                                                  "- Need to change your groups? You will need to run this command again.\n"
+                                                                  f"- It's recommended to give your cases IDs! You'll be able to run ``{prefix}case lookup`` to find its information.\n"
+                                                                  f"- You will need to add case members with ``{prefix}case add`` and assign them to a group. Remove them with ``{prefix}case remove``.\n"
+                                                                  f"- Run ``{prefix}case end`` inside your case chat to archive the case.\n"
+                                                                  f"- If you manually delete case channels, then you will need to run ``{prefix}case cleanup`` to free them from the database; otherwise, they will count against your case limit.\n"
+                                                                  f"- Case members can be easily muted/unmuted with ``{prefix}case mute`` or ``{prefix}case unmute``.")
 
+        await response.send(embed=embed)
 
         await response.success(f"Successfully saved your **Court add-on!** Now, judges (people with the {', '.join([r.mention for r in judge_roles])} role(s)) will be "
-                               f"able to run ``{prefix}case create`` to create a new case chat.") # TODO: set AllowedMentions so the bot doesn't role ping anyone
-
+                               f"able to run ``{prefix}case create`` to create a new case chat.", allowed_mentions=AllowedMentions(roles=False))
